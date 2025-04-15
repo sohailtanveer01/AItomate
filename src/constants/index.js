b3fbf3c151a0e1a9583ebf98ca777299033485d3
sohail tanveer <sohailtanveer769@gmail.com>
2025-04-14 20:34:23 -0500

    new icons

--- src/constants/index.js
+++ src/constants/index.js
@@ -90,7 +90,7 @@ export const benefits = [
     text: "Build intelligent chatbots for your business",
     details: "Create sophisticated AI-powered chatbots tailored to your business needs and customer interactions.",
     backgroundUrl: "./src/assets/benefits/card-1.svg",
-    iconUrl: "./src/assets/benefits/chatbot_17115944.png",
+    iconUrl: "./src/assets/benefits/chatbot (1).png",
     imageUrl: "https://raw.githubusercontent.com/adrianhajdin/project_ai_mern_image_generation/main/client/src/assets/robot.png",
   },
   {
@@ -99,7 +99,7 @@ export const benefits = [
     text: "Streamline reduntant tasks with AI",
     details: "AI-driven automation to optimize workflows, reduce manual effort, and enhance efficiency. Implement smart solutions to handle repetitive tasks, allowing your team to focus on strategic growth and innovation",
     backgroundUrl: "./src/assets/benefits/card-2.svg",
-    iconUrl: "data:image/svg+xml,%3Csvg width='800px' height='800px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' stroke='%238f549a'%3E%3Cpath d='M15.5 9L15.6716 9.17157C17.0049 10.5049 17.6716 11.1716 17.6716 12C17.6716 12.8284 17.0049 13.4951 15.6716 14.8284L15.5 15' stroke='%23ffffff' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M13.2942 7.17041L12.0001 12L10.706 16.8297' stroke='%23ffffff' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M8.49994 9L8.32837 9.17157C6.99504 10.5049 6.32837 11.1716 6.32837 12C6.32837 12.8284 6.99504 13.4951 8.32837 14.8284L8.49994 15' stroke='%23ffffff' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7' stroke='%23ffffff' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E",
+    iconUrl: "./src/assets/benefits/web-programming.png",
     imageUrl: benefitImage2,
     light: true,
   },
